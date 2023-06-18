48037889bbb194809bf6bd9a69cc843b9aa80e2c
정태승 <hi563@g.skku.edu>
2023-06-18 21:44:58 +0900

    ViewPager Indicator(TabLayout이용) 완성

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TableLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
@@ -25,7 +26,7 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
-        val viewpagerIndicator = view.findViewById<TableLayout>(R.id.viewpager_home_banner_indicator)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
         val assetLoader = AssetLoader()
         val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
@@ -48,5 +49,23 @@ class HomeFragment : Fragment() {
             viewpager.adapter = HomeBannerAdapter().apply {
                 submitList(homeData.topBanners)
             } // 인스턴스 생성과 동시에 데이터 전달
+
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin //Device 가로 길이 - 312dp - 16dp
+
+
+            viewpager.offscreenPageLimit = 3
+            viewpager.setPageTransformer { page, position ->
+                page.translationX = position * -offset // 왼쪽으로 이동(-)
+
+            }
+
+            TabLayoutMediator(viewpagerIndicator, viewpager) { tab, position ->
+
+             }.attach() // tablayout을 보이게하려면 필요!!
+
+
         }
     }}
