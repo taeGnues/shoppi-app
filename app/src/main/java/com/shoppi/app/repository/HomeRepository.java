b23695d644c3451caca6f7700f5a73572bd5de78
정태승 <hi563@g.skku.edu>
2023-06-20 18:44:49 +0900

    ViewModel 개요 - HomeViewModel 생성 및HomeFragment 수정하기

---
+++ app/src/main/java/com/shoppi/app/repository/HomeRepository.java
@@ -0,0 +1,4 @@
+package com.shoppi.app.repository;
+
+public class HomeRepository { // Repository는 데이터 소스로부터 데이터를 받아
+}
--- app/src/main/java/com/shoppi/app/ui/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.shoppi.app
+package com.shoppi.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.shoppi.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
